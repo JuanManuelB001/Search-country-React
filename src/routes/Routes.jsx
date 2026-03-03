4a495ddb0df72e8dd8d9685480da9aa2625ed056
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-03-03 13:30:54 -0500

    configurate gh-pages routes.jsx, packages.json and vite.config.json

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { SearchPage } from "../pages/SearchPage";
 import { DetailsCountry } from "../pages/DetailsCountry";
 export function MyRoutes(){
